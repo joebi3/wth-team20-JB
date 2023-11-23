ad2e641d0042a19d1e9f7f9c21a9d6d975ac91d5
joebi3 <joe@bi3.co>
2023-11-23 15:10:48 +0000

    Update Startup.cs

--- Application/src/RazorPagesTestSample/Startup.cs
+++ Application/src/RazorPagesTestSample/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RazorPagesTestSample.Data;
-
+//hello world
 namespace RazorPagesTestSample
 {
     public class Startup
